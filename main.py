b7a0ce8067530ff10bc2364e0ba8c0dab9f464c1
Henrique Leote <henriqueleote@outlook.com>
2023-05-30 19:57:43 +0200

    check_status and other fixes

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 import json
 from bs4 import BeautifulSoup
 from telegram.ext import Updater, CommandHandler, CallbackContext
-from telegram import Update
+from telegram import Update, Chat, Message
 from selenium import webdriver
-from selenium.webdriver.firefox.options import Options
 from datetime import datetime
 import os, psutil
 
@@ -50,29 +49,31 @@ def help(update: Update, context: CallbackContext):
     chat_id = update.effective_chat.id
     context.bot.send_message(chat_id=chat_id, text='Welcome!\nUse /interval <seconds> to set the interval.\nUse /run to start afeter setting an interval.\nUser /stop to stop updates.')
 
-# Handle the /stop command
-def stop(update: Update, context: CallbackContext):
+
+# Handle the /status command
+def check_status(update: Update, context: CallbackContext):
     chat_id = update.effective_chat.id
+
     if str(chat_id) in user_settings:
-        if user_settings[str(chat_id)].get("status") == "running":
-            # Remove the job for the user
-            job = context.job_queue.get_jobs_by_name(str(chat_id))
-            if job:
-                job[0].schedule_removal()
-                user_settings[str(chat_id)]["status"] = "stopped"
-                save_settings()
-                context.bot.send_message(chat_id=chat_id,text='Updates have stopped.\nUse /run to start getting updates')
-                now = datetime.now()
-                dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
-                print(update.effective_chat.username + " stopped the updates at " + dt_string)
-            else:
-                context.bot.send_message(chat_id=chat_id, text='No interval is set.')
+        now = datetime.now()
+        dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
+
+        # check if user has username
+        if update.effective_chat.username is not None:
+            print(update.effective_chat.username + ' checked status at ' + dt_string)
         else:
-            context.bot.send_message(chat_id=chat_id, text='Updates were already stopped.')
-    else:
-        context.bot.send_message(chat_id=chat_id, text='No updates were found for this user')
+            print(str(chat_id) + '  checked status at  ' + dt_string)
 
+        if (user_settings[str(chat_id)].get('status') == "running"):
+            interval = user_settings[str(chat_id)].get("interval")
+            if interval % 60 == 0:
+                message = f'Updates are currently running every {interval // 60} minute(s).'
+            else:
+                message = f'Updates are currently running every {interval} seconds.'
+        else:
+            message = "Updates are currently stopped."
 
+        context.bot.send_message(chat_id=chat_id, text=message)
 
 # Handle the /interval command
 def set_interval(update: Update, context: CallbackContext):
@@ -88,6 +89,8 @@ def set_interval(update: Update, context: CallbackContext):
             job = context.job_queue.get_jobs_by_name(str(chat_id))
             if job:
                 job[0].schedule_removal()
+                user_settings[str(chat_id)]["status"] = "stopped"
+
 
         # Check if a key exists in the dictionary
         if not str(chat_id) in user_settings:
@@ -101,16 +104,26 @@ def set_interval(update: Update, context: CallbackContext):
         user_settings[str(chat_id)]["interval"] = interval
         save_settings()
 
-        context.bot.send_message(chat_id=chat_id, text=f'Interval set to {interval} seconds.')
+        now = datetime.now()
+        dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
+
+        # check if user has username
+        if update.effective_chat.username is not None:
+            print(update.effective_chat.username + ' set a new interval at ' + dt_string)
+        else:
+            print(str(chat_id) + '  set a new interval at ' + dt_string)
+
+        run(update, context)
 
     except (IndexError, ValueError):
         context.bot.send_message(chat_id=chat_id, text='Wrong instruction, please write /interval <seconds>')
 
 # Handle the /grade & /allproducts command
+#link is not changing
 def set_link(update: Update, context: CallbackContext):
     chat_id = update.effective_chat.id
     message = ''
-    try:
+    if update.effective_message.text == '/allproducts' or update.effective_message.text == '/grade':
         if update.effective_message.text == '/allproducts':
             link = URL_ALL
             message = f"Now getting updates from all products"
@@ -118,44 +131,45 @@ def set_link(update: Update, context: CallbackContext):
             link = URL_GRADE
             message = f"Now getting updates from Grade A/B/C products"
 
-        # Check if the user already has an interval set
-        if str(chat_id) in user_settings:
-            # Remove the previous job
-            job = context.job_queue.get_jobs_by_name(str(chat_id))
-            if job:
-                job[0].schedule_removal()
-
         # Check if a key exists in the dictionary
         if not str(chat_id) in user_settings:
             user_settings[str(chat_id)] = {
-                "interval": DEFAULT_INTERVAL,
-                "link": link,
-                "most_recent": "",
-                "status": "stopped"
+               "interval": DEFAULT_INTERVAL,
+               "link": URL_ALL,
+               "most_recent": "",
+               "status": "stopped"
             }
 
         user_settings[str(chat_id)]["link"] = link
         save_settings()
-
         context.bot.send_message(chat_id=chat_id, text=message)
 
-    except (IndexError, ValueError):
-        context.bot.send_message(chat_id=chat_id, text='Please don\'t write anything else')
+        now = datetime.now()
+        dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
+
+        # check if user has username
+        if update.effective_chat.username is not None:
+            print(update.effective_chat.username + ' set a new link at ' + dt_string)
+        else:
+            print(str(chat_id) + '  set a new link at ' + dt_string)
+    else:
+        context.bot.send_message(chat_id=chat_id, text='Wrong instruction')
+
 
 # Loads the browser and gets the most recent product
-def online_search(chat_id,link):
+def online_search(chat_id):
+    link = user_settings[str(chat_id)].get("link")
     most_recent = user_settings[str(chat_id)].get("most_recent")
     new_product_count = -1
-    options = Options()
-    options.add_argument("--headless")
-    options.add_argument("--disable-gpu")
-    options.add_argument("--no-sandbox")
-    options.add_argument("--disable-dev-shm-usage")
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--disable-gpu")
+    driver = webdriver.Chrome(options=chrome_options)
 
     # Create the WebDriver instance
-    driver = webdriver.Firefox(options=options)
     driver.get(link)
-    driver.implicitly_wait(1)  # Wait up to 2 seconds for elements to appear
+    #driver.implicitly_wait(1)  # Wait up to 2 seconds for elements to appear
 
     html_content = driver.page_source
     driver.quit()
@@ -176,16 +190,15 @@ def online_search(chat_id,link):
             product = li_element['data-product-quickshop-url']
             product_price = li_element.find("span", {"class", "money"}).text
             product_name = li_element.find("h2", {"class", "productitem--title"}).text
+            now = datetime.now()
+            dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
             if product != most_recent:
+
                 # Product data
                 most_recent = product
                 productID = most_recent.split('/')[4]
                 user_settings[str(chat_id)]["most_recent"] = most_recent
                 save_settings()
-
-                now = datetime.now()
-                dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
-                print(f"{chat_id} : {product_name} -> {product_price} | {dt_string}")
 
                 # returns a message with the new product
                 if new_product_count == 1 or new_product_count == -1:
@@ -194,9 +207,38 @@ def online_search(chat_id,link):
                     message = f'{product_name} -> {product_price}\nhttps://talk-point.de/products/{productID}\nNovos produtos: {new_product_count}'
                 return message
             else:
-                print(f"{chat_id} -> no product")
+                print(f"{chat_id} -> no product | {dt_string}")
     else:
         print('Error: ul_element not found')
+
+# Handle the /stop command
+def stop(update: Update, context: CallbackContext):
+    chat_id = update.effective_chat.id
+    if str(chat_id) in user_settings:
+        if user_settings[str(chat_id)].get("status") == "running":
+
+            # Remove job
+            job = context.job_queue.get_jobs_by_name(str(chat_id))
+            if job:
+                job[0].schedule_removal()
+
+            user_settings[str(chat_id)]["status"] = "stopped"
+            save_settings()
+            context.bot.send_message(chat_id=chat_id,text='Updates have stopped.\nUse /run to start getting updates')
+            now = datetime.now()
+            dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
+
+            # check if user has username
+            if update.effective_chat.username is not None:
+                print(update.effective_chat.username + ' stopped the updates at ' + dt_string)
+            else:
+                print(str(chat_id) + ' stopped the updates at ' + dt_string)
+        else:
+            context.bot.send_message(chat_id=chat_id, text='Updates were already stopped.')
+    else:
+        context.bot.send_message(chat_id=chat_id, text='No updates were found for this user')
+
+
 
 
 # Handle the data function to be executed with the specified interval
@@ -227,21 +269,29 @@ def run(update: Update, context: CallbackContext):
 
     if user_settings[str(chat_id)].get("status") == "stopped":
         interval = user_settings[str(chat_id)].get("interval")
-        link = user_settings[str(chat_id)].get("link")  # Use .get() to handle missing link case
-        context.bot.send_message(chat_id=chat_id, text=f'Updates have started every {interval} seconds.')
+        if interval % 60 == 0:
+            context.bot.send_message(chat_id=chat_id, text=f'Updates have started every {interval // 60} minute(s).')
+        else:
+            context.bot.send_message(chat_id=chat_id, text=f'Updates have started every {interval} seconds.')
+
         now = datetime.now()
         dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
-        print(update.effective_chat.username + " joined the updates at " + dt_string)
+
+        #check if user has username
+        if update.effective_chat.username is not None:
+            print(update.effective_chat.username + ' joined the updates at ' + dt_string)
+        else:
+            print(str(chat_id) + ' joined the updates at ' + dt_string)
         user_settings[str(chat_id)]["status"] = "running"
         save_settings()
-        context.job_queue.run_repeating(notify, interval, context=(chat_id, link), name=str(chat_id))
+        context.job_queue.run_repeating(notify, interval, context=(chat_id), name=str(chat_id))
     else:
         context.bot.send_message(chat_id=chat_id, text='Updates were already running.')
 
 # Notifies the user with data from the function online_search
 def notify(context: CallbackContext):
-    chat_id, link = context.job.context
-    message = online_search(chat_id,link)
+    chat_id = context.job.context
+    message = online_search(chat_id)
     # if there's a message, sends to the user
     if message:
         context.bot.send_message(chat_id=chat_id, text=message, parse_mode='Markdown')
@@ -270,15 +320,15 @@ def main():
     dispatcher.add_handler(CommandHandler('grade', set_link))
     dispatcher.add_handler(CommandHandler('allproducts', set_link))
     dispatcher.add_handler(CommandHandler('run', run))
+    dispatcher.add_handler(CommandHandler('status', check_status))
 
     # Start the bot
     updater.start_polling()
 
     # Send a message to all users
-    #for chat_id in user_settings.keys():
-    #    updater.bot.send_message(chat_id=chat_id, text='The bot has restarted.\nPlease /run to continue getting updates')
+    for chat_id in user_settings.keys():
+        updater.bot.send_message(chat_id=chat_id, text='The bot has restarted.\nPlease /run to continue getting updates')
 
-    process = psutil.Process()
     print(psutil.Process(os.getpid()).memory_info().rss / 1024 ** 2)
 
     updater.idle()
