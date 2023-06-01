b2d12036137f295876bbc5814223b42e3c7cd8f3
Henrique Leote <henriqueleote@outlook.com>
2023-06-01 19:10:59 +0200

    Comment code

--- TalkPointChannelBot.py
+++ TalkPointChannelBot.py
@@ -14,6 +14,7 @@ channel_id = "-1001921638321"
 
 "10089055-225"
 
+# Function to get the most recent URL from the file
 def get_most_recent():
     try:
         with open(MOST_RECENT_FILE, 'r') as file:
@@ -21,10 +22,12 @@ def get_most_recent():
     except FileNotFoundError:
         return None
 
+# Function to set the most recent URL in the file
 def set_most_recent(value):
     with open(MOST_RECENT_FILE, 'w') as file:
         file.write(value)
 
+# Function to run the WebDriver and retrieve the HTML content of the page
 def runWebDriver(url):
     chrome_options = webdriver.ChromeOptions()
     chrome_options.add_argument("--no-sandbox")
@@ -38,26 +41,27 @@ def runWebDriver(url):
     soup = BeautifulSoup(html_content, 'html.parser')
     return soup;
 
+# Function to fetch data from the website and send updates to the Telegram channel
 def getData(bot):
     print('running...')
     most_recent = get_most_recent()
 
-    #Get internet code
+    # Get internet code
     soup = runWebDriver(URL_GRADE)
     ul_element = soup.select_one('.boost-pfs-filter-products')
 
     new_products = []
 
-    # checks how many products were added since the last "most_recent" until the newest
+    # Check how many products were added since the last "most_recent" until the newest
 
-    #TODO if doesnt exist, just show the newest
+    # TODO: If it doesn't exist, just show the newest
     for index, element in enumerate(ul_element):
         if element['data-product-quickshop-url'] == most_recent:
-            #new_product_count = index
+            # new_product_count = index
             break
         new_products.append(element)
 
-    # find the most_recent through the HTML <li> inside the <ul>
+    # Find the most recent through the HTML <li> inside the <ul>
     if ul_element:
         li_element = ul_element.find('li')
         if li_element:
@@ -95,8 +99,8 @@ def getData(bot):
                     product_name = product_li.find("h2", {"class", "productitem--title"}).text
                     title = f'{blue_circle}{white_circle} Talkpoint {white_circle}{blue_circle}\n'
                     message = f'{title}{product_name}\nPrice: {product_price}\nCondition: {condition} {emoji}\nhttps://talk-point.de/products/' + productID
-                    if(img_src != ''):
-                        bot.send_photo(chat_id=channel_id, photo=img_src,caption=message)
+                    if (img_src != ''):
+                        bot.send_photo(chat_id=channel_id, photo=img_src, caption=message)
                     else:
                         bot.send_message(chat_id=channel_id, text=message)
                     time.sleep(2)
