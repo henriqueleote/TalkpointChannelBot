8ed640f3ac6ca58c03d25d5963d8b02b791daf5e
Henrique Leote <henriqueleote@outlook.com>
2023-06-01 18:36:00 +0200

    Showing grade, image and driver in seperate function

--- talkpoint_channel_bot.py
+++ talkpoint_channel_bot.py
@@ -4,6 +4,8 @@ from selenium import webdriver
 import telegram
 import re
 
+from telegram import ParseMode
+
 # URL of the website you want to fetch
 URL_ALL = 'https://talk-point.de/search?type=article%2Cpage%2Cproduct&q=&sort=created-descending'
 URL_GRADE = 'https://talk-point.de/collections/all?sort=created-descending&pf_t_produktzustand=Zustand_A&pf_t_produktzustand=Zustand_B&pf_t_produktzustand=Zustand_C'
@@ -26,25 +28,32 @@ def set_most_recent(value):
     with open(MOST_RECENT_FILE, 'w') as file:
         file.write(value)
 
-def getData():
-    most_recent = get_most_recent()
-    new_product_count = -1
+def runWebDriver(url):
     chrome_options = webdriver.ChromeOptions()
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--headless")
     chrome_options.add_argument("--disable-gpu")
     driver = webdriver.Chrome(options=chrome_options)
     # Create the WebDriver instance
-    driver.get(URL_GRADE)
+    driver.get(url)
     html_content = driver.page_source
     driver.quit()
-
     soup = BeautifulSoup(html_content, 'html.parser')
+    return soup;
+
+def getData(bot):
+    print('running...')
+    most_recent = get_most_recent()
+
+    #Get internet code
+    soup = runWebDriver(URL_GRADE)
     ul_element = soup.select_one('.boost-pfs-filter-products')
 
     new_products = []
 
     # checks how many products were added since the last "most_recent" until the newest
+
+    #TODO if doesnt exist, just show the newest
     for index, element in enumerate(ul_element):
         if element['data-product-quickshop-url'] == most_recent:
             #new_product_count = index
@@ -55,44 +64,45 @@ def getData():
     if ul_element:
         li_element = ul_element.find('li')
         if li_element:
-            bot = telegram.Bot(token=TOKEN)
             if new_products:
                 res = new_products[::-1]  # reversing using list slicing
-                for li_element in res:
+                for product_li in res:
 
-                    #Gets product grade state
-                    #element_html = str(li_element)
-                    script = '''
-                    var style = getComputedStyle(document.querySelector('.product-condition-scale-overlay > div:nth-child(3)'), '::before');
-                    return style.getPropertyValue('content') + "hello";
-                    '''
+                    product_item = product_li['data-product-quickshop-url']
+                    image = product_li.find('img', 'productitem--image-primary')
+                    productID = product_item.split('/')[4]
 
-                    #pre_grade_state = driver.execute_script(script, element_html)
-                    grade_state = ''#pre_grade_state.strip('"')
-                    emoji = ''
-                    condition = ''
+                    img_src = ''
 
-                    #print(grade_state)
+                    grade = productID[-3:]
 
-                    if(grade_state == "sehr gut"):
-                        emoji = '\U0001F7E0'
-                        condition = 'Very good'
-                    if (grade_state == "gut"):
-                        emoji = '\U0001F7E1'
-                        condition = 'Good'
-                    if (grade_state == "wie neu"):
-                        emoji = '\U0001F7E2'
-                        condition = 'Like new'
+                    pre_img_src = image.get('src')
+                    if pre_img_src[0] == "/":
+                        img_src = pre_img_src[2:]
+                    else:
+                        img_src = pre_img_src
 
-                    product = li_element['data-product-quickshop-url']
-                    product_price = li_element.find("span", {"class", "money"}).text
-                    product_name = li_element.find("h2", {"class", "productitem--title"}).text
-                    productID = product.split('/')[4]
-                    message = f'{product_name} -> {product_price}\nhttps://talk-point.de/products/' + productID
-                    print(message)
-                    print(product)
-                    bot.send_message(chat_id=channel_id, text=message)
-                    time.sleep(3)
+                    if (grade == "223"):
+                        emoji = "\U0001F7E2"
+                        condition = "Like new"
+                    if (grade == "224"):
+                        emoji = "\U0001F7E1"
+                        condition = "Very good"
+                    if (grade == "225"):
+                        emoji = "\U0001F7E0"
+                        condition = "Good"
+
+                    blue_circle = "\U0001F535"
+                    white_circle = "\u26AA"
+                    product_price = product_li.find("span", {"class", "money"}).text
+                    product_name = product_li.find("h2", {"class", "productitem--title"}).text
+                    title = f'{blue_circle}{white_circle} Talkpoint {white_circle}{blue_circle}\n'
+                    message = f'{title}{product_name}\nPrice: {product_price}\nCondition: {condition} {emoji}\nhttps://talk-point.de/products/' + productID
+                    if(img_src != ''):
+                        bot.send_photo(chat_id=channel_id, photo=img_src,caption=message)
+                    else:
+                        bot.send_message(chat_id=channel_id, text=message)
+                    time.sleep(2)
                 set_most_recent(new_products[0]['data-product-quickshop-url'])
                 new_products.clear()
                 res.clear()
@@ -101,8 +111,9 @@ def getData():
     else:
         print('Error: ul_element not found')
 
-    #driver.quit()
+bot = telegram.Bot(token=TOKEN)
+bot.send_message(chat_id=channel_id, text='Now getting updates from Talkpoint')
 
 while (True):
-    getData()
+    getData(bot)
     time.sleep(180)
